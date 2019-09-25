7041909c490a90b67ac5588354c645c4c0bfb77d
Zaenal Anzarry <55571352+zaenalanzarry@users.noreply.github.com>
2019-09-25 14:40:29 +0700

    Add files via upload

---
+++ IF1-10118023-Latihan7-Integer/src/latihan7/Latihan7.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package latihan7;
+
+/**
+ * @author Zaenal PC
+ * NAMA              : Zaenal Anzarry
+ * KELAS             : IF-01
+ * NIM               : 10118023
+ * Deskripsi Program : Integer
+ */
+
+public class Latihan7 {
+
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + l);
+    }
+    
+}
